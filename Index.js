f1a5485e07c7c5efb5de8227f198316f0ecc7e82
harjunpnik <niklasharjunpaa@gmail.com>
2018-12-18 19:27:46 +0200

    First commit

---
+++ Index.js
@@ -0,0 +1,75 @@
+//Loads libraries: discord.js 
+const Discord = require('discord.js');
+
+//Sets your client, named to : bot.
+const bot = new Discord.Client();
+
+//Prefix to messages so the bot will understand to execute the code.
+const prefix = '-';
+
+//Bot does this on start-up.
+bot.on('ready', () => {
+    //on successfull startup it writes in the console log 'I am ready!'.
+    console.log('I am ready!');
+    //user.setGame(""); Sets the game what bot is playing. Can be used to display usefull messages.
+    bot.user.setActivity("My prefix is: -");
+});
+
+
+//This will run each time a messages is written to the chat.
+bot.on('message', message => {
+
+    //Creating variables
+    let msg = message.content.toUpperCase();                    // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces.
+
+    //This function checks if the sender of the message is the bot, it returns so that the bot won't loop with itself.
+    if(message.author.bot) return;
+    
+    //This function checks if the message has a prefix in the beginning, if not it returns.*-
+    if(message.content.indexOf(prefix) !== 0) return;
+    
+
+    //--------COMMANDS--------
+    
+    //--------HELP------------
+    if(msg === prefix + 'HELP'){
+        //Makes bot send help message to show users the commands.
+        message.channel.send(
+                        "Here is a list of my commands: \n" + 
+                        "-genre     ---- Recommends a random music genre to listen to. \n" +
+                        "-help  ---- Shows all the commands. \n" +
+                        "-ping   ---- Bot replys Pong! \n" +
+                        "-roll    ---- Rolls a number between 1 and 100. \n" 
+                        );
+    }
+    
+    //--------GENRE-----------
+    if (msg === prefix + 'GENRE'){
+        //Posts a random genre recommendation.
+        
+        //Creating random number variable and genre array
+        var genreNumber;
+        var genre = ["Blues", "Classical", "Country", "Folk", "Jazz", "Newage", "Reggae", "Rock"];
+        //Calculating random number based on array size.
+        genreNumber = Math.floor((Math.random() * genre.length));
+        //Posts the genre recommendation.
+        message.reply(genre[genreNumber]);  
+    }
+    
+     //-------PING------------
+    if (msg === prefix + 'PING') {
+        //Simple commmand that will post a message back, "Pong!" in this case.
+        message.channel.send('Pong!');
+    }
+    
+    //--------ROLL------------
+    if (msg === prefix + 'ROLL'){
+        //Rolls a random number between 1 and 100
+        message.reply(Math.floor((Math.random() * 100) + 1));
+    }
+    
+});
+
+//Your bot token for the bot so that it can connect to the server.
+bot.login('<Your Token goes here>');
